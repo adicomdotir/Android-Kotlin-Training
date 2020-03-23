501ec371f7f01b31a39ee91d44bc2d840a87bf83
adicomdotir <adicom.ir@gmail.com>
2020-03-23 16:05:14 +0430

    Add fragment

---
+++ app/src/main/java/ir/adicom/app/diceroller/TriviaActivity.kt
@@ -0,0 +1,12 @@
+package ir.adicom.app.diceroller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TriviaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trivia)
+    }
+}
